90fe0dc9a1658d244efbeff40897265741bc981d
Lennart <len397a@gmx.de>
2014-08-13 14:01:17 +0200

    anfang such funktion

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -43,9 +43,33 @@ class SiteController extends Controller
 		// {
 			// print_r('error: '.$e);    
 		// }
-		$model = Books::model()->published()->recently()->with('bookgenres')->findAll();
-		
-		$this->render('index',array('books' => $model));
+        
+        $text = '';
+        $model = new Books('search');
+		if(isset($_GET['Books'])) {
+            $text = $_GET['Books']['title'];
+            if(!empty($text)){
+                $model->unsetAttributes();
+                $model->description = $text;   // any filtering value that you want to apply
+                $model->title = $text;
+                $model->status = 1;
+                $dataProvider = $model->search();
+                $model2 = Books::model();
+                $this->render('index',array('dataProvider' => $dataProvider, 'model2'=>$model2));
+            }
+            else
+            {
+                $model2 = Books::model();
+                $model = Books::model()->published()->recently()->with('bookgenres')->findAll();
+                $this->render('index',array('books' => $model, 'model2'=>$model2));
+            }
+        }
+        else
+        {
+            $model2 = Books::model();
+            $model = Books::model()->published()->recently()->with('bookgenres')->findAll();
+            $this->render('index',array('books' => $model, 'model2'=>$model2));
+        }
 	}
 
 	/**
