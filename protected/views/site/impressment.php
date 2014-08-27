5c5134e1616156a5c9c23df8d8273f71edb5c910
Florian Jacobsen <fjacobsen90@gmail.com>
2014-08-27 13:29:02 +0200

    added impressment site

---
+++ protected/views/site/impressment.php
@@ -0,0 +1,123 @@
+<section class="text-disclaimer">
+    <div class="container">
+        <div class="row">
+
+            <h3>Impressum</h3>
+
+            <ul>
+                <li class="text-muted">Semesterprojekt</li>
+                <li class="text-muted">Sommer Semester 2014</li>
+                <li class="text-muted">Fh Flensburg</li>
+            </ul>
+
+            <h4>Verantwortlich für den Inhalt dieser Website</h4>
+            
+            <ul>
+                <li>Lennart Behncke
+                    <span class="label label-inf">Angewandte Informatik</span>
+                    <span class="label label-proto">#510040</span>
+                    <span class="label label-cred">Backend Programmierung</span>
+                    <span class="label label-cred">Web-Programmierung</span>
+                    <span class="label label-cred">PHP - Yii Framework</span>
+                </li>
+                <li>Alexander Hamburg
+                    <span class="label label-inf">Medieninformatik</span>
+                    <span class="label label-proto">#510851</span>
+                    <span class="label label-cred">Backend Programmierung</span>
+                    <span class="label label-cred">Web-Programmierung</span>
+                    <span class="label label-cred">PHP - Yii Framework</span>
+                </li>
+                <li>Julian Haseleu
+                    <span class="label label-inf">Medieninformatik</span>
+                    <span class="label label-proto">#510000</span>
+                    <span class="label label-cred">Konzeption und Gestaltung</span>
+                    <span class="label label-cred">Backend Programmierung</span>
+                    <span class="label label-cred">Serveradministration</span>
+                </li>
+                <li>Florian Jacobsen
+                    <span class="label label-inf">Medieninformatik</span>
+                    <span class="label label-proto">#510834</span>
+                    <span class="label label-cred">Projektmanagement</span>
+                    <span class="label label-cred">Konzeption und Gestaltung</span>
+                    <span class="label label-cred">Web-Gestaltung (HTML, CSS)</span>
+                    <span class="label label-cred">Bootstrap</span>
+                    <span class="label label-cred">Dokumentation</span>
+                </li>
+                <li>Sebastian Mondry
+                    <span class="label label-inf">Medieninformatik</span>
+                    <span class="label label-proto">#510785</span>
+                    <span class="label label-cred">Konzeption und Gestaltung</span>
+                    <span class="label label-cred">Dokumentation</span>
+                </li>
+
+                <li class="pushMe">Unterstützt durch den betreudenden Dozenten <b>Tobias Hiep</b><span class="glyphicon glyphicon-ok"></span></li>
+            </ul>
+
+
+            <h4>Kontakt</h4>
+            <a href="#">info@bookworks.noip.me</a></li>
+        </div>
+    </div>
+</section>
+
+<section class="text-disclaimer">
+    <div class="container">
+        <div class="row">
+
+            <h3>Haftungsausschluss <span class="label label-proto">Prototyp</span></h3>
+
+            <h4>1. Haftungsbeschränkung</h4>
+            <p class="text-disclaimer-small">
+            Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt erstellt. Der Anbieter übernimmt jedoch keine Gewähr für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Inhalte. Die Nutzung der Inhalte der Website erfolgt auf eigene Gefahr des Nutzers. Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors und nicht immer die Meinung des Anbieters wieder. Mit der reinen Nutzung der Website des Anbieters kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande.
+            </p>
+
+            <h4>2. Externe Links</h4>
+            <p class="text-disclaimer-small">
+            Diese Website enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites unterliegen der Haftung der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der externen Links die fremden Inhalte daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße ersichtlich. Der Anbieter hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der verknüpften Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis oder Link liegenden Inhalte zu Eigen macht. Eine ständige Kontrolle der externen Links ist für den Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch derartige externe Links unverzüglich gelöscht.
+            </p>
+            <h4>3. Urheber- und Leistungsschutzrechte</h4>
+            <p class="text-disclaimer-small">
+            Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen Urheber- und Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei als solche gekennzeichnet. Die unerlaubte Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter Seiten ist nicht gestattet und strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht kommerziellen Gebrauch ist erlaubt. Die Darstellung dieser Website in fremden Frames ist nur mit schriftlicher Erlaubnis zulässig.
+            </p>
+            <h4>4. Datenschutz</h4>
+            <p class="text-disclaimer-small">
+            Durch den Besuch der Website des Anbieters können Informationen über den Zugriff (Datum, Uhrzeit, betrachtete Seite) gespeichert werden. Diese Daten gehören nicht zu den personenbezogenen Daten, sondern sind anonymisiert. Sie werden ausschließlich zu statistischen Zwecken ausgewertet. Eine Weitergabe an Dritte, zu kommerziellen oder nichtkommerziellen Zwecken, findet nicht statt. Der Anbieter weist ausdrücklich darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen und nicht lückenlos vor dem Zugriff durch Dritte geschützt werden kann. Die Verwendung der Kontaktdaten des Impressums zur gewerblichen Werbung ist ausdrücklich nicht erwünscht, es sei denn der Anbieter hatte zuvor seine schriftliche Einwilligung erteilt oder es besteht bereits eine Geschäftsbeziehung. Der Anbieter und alle auf dieser Website genannten Personen widersprechen hiermit jeder kommerziellen Verwendung und Weitergabe ihrer Daten.
+            <br><br>
+            <b>Personenbezogene Daten</b><br><br>
+            Sie können unsere Webseite ohne Angabe personenbezogener Daten besuchen. Soweit auf unseren Seiten personenbezogene Daten (wie Name, Anschrift oder E-Mail Adresse) erhoben werden, erfolgt dies, soweit möglich, auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. Sofern zwischen Ihnen und uns ein Vertragsverhältnis begründet, inhaltlich ausgestaltet oder geändert werden soll oder Sie an uns eine Anfrage stellen, erheben und verwenden wir personenbezogene Daten von Ihnen, soweit dies zu diesen Zwecken
+            erforderlich ist (Bestandsdaten). Wir erheben, verarbeiten und nutzen personenbezogene Daten soweit dies erforderlich ist, um Ihnen die Inanspruchnahme des Webangebots zu ermöglichen (Nutzungsdaten). Sämtliche personenbezogenen Daten werden nur solange gespeichert wie dies für den geannten Zweck (Bearbeitung Ihrer Anfrage oder Abwicklung eines Vertrags) erforderlich ist. Hierbei werden steuer- und handelsrechtliche Aufbewahrungsfristen berücksichtigt. Auf Anordnung der zuständigen Stellen dürfen wir im Einzelfall Auskunft über diese Daten (Bestandsdaten) erteilen, soweit dies für Zwecke der Strafverfolgung, zur Gefahrenabwehr, zur Erfüllung der gesetzlichen Aufgaben der Verfassungsschutzbehörden oder des Militärischen Abschirmdienstes oder zur Durchsetzung der Rechte am geistigen Eigentum erforderlich ist.
+            <br><br>
+            <b>Kommentarfunktionen</b><br><br>
+            Im Rahmen der Kommentarfunktion erheben wir personenbezogene Daten (z.B. Name, E-Mail) im Rahmen Ihrer Kommentierung zu einem Beitrag nur in dem Umfang wie Sie ihn uns mitgeteilt haben. Bei der Veröffentlichung eines Kommentars wird die von Ihnen angegebene Email-Adresse gespeichert, aber nicht veröffentlicht. Ihr Name wird veröffentlicht, wenn Sie nicht unter Pseudonym geschrieben haben.
+            <br><br>
+            <b>Datenschutzerklärung für das Facebook-Plugin („Gefällt mir“)</b><br><br>
+            Diese Webseite nutzt Plugins des Anbieters Facebook.com, welche durch das Unternehmen Facebook Inc., 1601 S. California Avenue, Palo Alto, CA 94304 in den USA bereitgestellt werden. Nutzer unserer Webseite, auf der das Facebook-Plugin („Gefällt mir“-Button) installiert ist, werden hiermit darauf hingewiesen, dass durch das Plugin eine Verbindung zu Facebook aufgebaut wird, wodurch eine Übermittlung an Ihren Browser durchgeführt wird, damit das Plugin auf der Webseite erscheint. Des Weiteren werden durch die Nutzung Daten an die Facebook-Server weitergeleitet, welche Informationen über Ihre Webseitenbesuche auf unserer Homepage enthalten. Dies hat für eingeloggte Facebook-Nutzer zur Folge, dass die Nutzungsdaten Ihrem persönlichen Facebook-Account zugeordnet werden. Sobald Sie als eingeloggter Facebook-Nutzer aktiv das Facebook-Plugin nutzen (z.B. durch das Klicken auf den „Gefällt mir“ Knopf oder die Nutzung der Kommentarfunktion), werden diese Daten zu Ihrem Facebook-Account übertragen und veröffentlicht. Dies können Sie nur durch vorheriges Ausloggen aus Ihrem Facebook-Account umgehen. Weitere Information bezüglich der Datennutzung durch Facebook entnehmen Sie bitte den datenschutzrechtlichen Bestimmungen auf Facebook unter http://de-de.facebook.com/policy.php.
+            <br><br>
+            <b>Auskunftsrecht</b><br><br>
+            Sie haben das jederzeitige Recht, sich unentgeltlich und unverzüglich über die zu Ihrer Person erhobenen Daten zu erkundigen. Sie haben das jederzeitige Recht, Ihre Zustimmung zur Verwendung Ihrer angegeben persönlichen Daten mit Wirkung für die Zukunft zu widerrufen. Zur Auskunftserteilung wenden Sie sich bitte an den Anbieter unter den Kontaktdaten im Impressum.
+            </p>
+            <h4>5. Besondere Nutzungsbedingungen</h4>
+            <p class="text-disclaimer-small">
+            Soweit besondere Bedingungen für einzelne Nutzungen dieser Website von den vorgenannten Nummern 1. bis 4. abweichen, wird an entsprechender Stelle ausdrücklich darauf hingewiesen. In diesem Falle gelten im jeweiligen Einzelfall die besonderen Nutzungsbedingungen.
+            </p>
+            <h4>6. Jugendschutz</h4>
+            <p class="text-disclaimer-small">
+            Ungehinderte Zugänglichmachen von Pornographie in Deutschland unter Strafandrohung verboten, § 184 I StGB. Veröffentlichung von Pornographischen Schriften und/oder
+            Pornographischen Bildmaterial ist untersagt. Jegliche Zuwiderhandlung ist untersagt und geschieht gegen unser Wissen und wird geahndet.<br>
+            Veröffentlichung von Propagandamitteln, rechtem Gedankengut oder Rechter Schriften und/oder Bildmaterial ist untersagt. Jegliche Zuwiderhandlung ist untersagt und geschieht gegen unser Wissen und wird geahndet.
+            </p>
+        </div>
+    </div>
+</section>    
+
+<section class="text-disclaimer">
+    <div class="container">
+        <div class="row">
+
+            <h3>Allgemeine Geschäftsbedingungen (AGBs) <span class="label label-proto"> Prototyp</span></h3>
+            <p class="text-muted"> ... </p>
+
+
+        </div>
+    </div>
+</section>
